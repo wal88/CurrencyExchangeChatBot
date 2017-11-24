fcc826a024423a3f27273ff4e8756dee31dcbf0a
wal88 <waleedalsaady1@gmail.com>
2017-11-25 11:34:59 +1300

    working on using a cognitive api

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var restify = require('restify');
 var builder = require('botbuilder');
 var luis = require('./controller/LuisDialog');
+var microsoftComputerVision = require("microsoft-computer-vision");
+const fs = require('fs');
+
 
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
@@ -17,8 +20,47 @@ var connector = new builder.ChatConnector({
 server.post('/api/messages', connector.listen());
 
 
-var bot = new builder.UniversalBot(connector, function (session) {           
-   session.send('Sorry, I did not understand \'%s\'. Type \'help\' if you need assistance.', session.message.text)
+var bot = new builder.UniversalBot(connector, function (session) {
+    var msg = session.message;
+    if (msg.attachments && msg.attachments.length > 0) {
+
+        var attachment = msg.attachments[0];
+        //    session.send({
+        //        text: "You sent:",
+        //        attachments: [
+        //            {
+        //                contentType: attachment.contentType,
+        //                contentUrl: attachment.contentUrl,
+        //                name: attachment.name
+        //            }
+        //        ]
+        //    });
+        // session.send( "contentType: "+attachment.contentType
+        //                    +"\ncontentUrl: "+attachment.contentUrl
+        //                    +"\nname: "+attachment.name);
+        console.log(attachment.contentType);
+
+       microsoftComputerVision.orcImage({
+            "Ocp-Apim-Subscription-Key": "92c7a34041824d56a62bf3b15e831f7c",
+            "request-origin": "westcentralus",
+            "content-type": "application/octet-stream",
+            "url": attachment.contentUrl,
+            "language": "en",
+            "detect-orientation": true
+
+        }).then((result) => {
+
+            console.log(JSON.stringify(result));
+            session.send(JSON.stringify(result));
+            session.send(result);
+
+        }).catch((err) => {
+            throw err;
+        })
+
+    } else {
+        session.send('Sorry, I did not understand here is a list of what I can help you with: ', session.message.text);
+    }
 });
 
 luis.startDialog(bot);
